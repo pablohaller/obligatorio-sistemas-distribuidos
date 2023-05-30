565d440dfb813e2aaee350ff4917667d3f4bb388
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2023-05-30 12:40:03 -0300

    Trying backup

--- postgres-conf/init.sql
+++ postgres-conf/init.sql
@@ -1,3 +1,6 @@
+CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'my_replicator_password';
+SELECT * FROM pg_create_physical_replication_slot('replication_slot_slave1');
+
 CREATE TABLE IF NOT EXISTS Mediciones (
     datetime TIMESTAMP(0),
     sensor VARCHAR(255),
